c66eb6273f3e838af50bbb2f722bb50248cbc8b2
nurik1347 <nurikmirzaev1347@gmail.com>
2024-03-19 15:11:56 +0500

    done

---
+++ main.js
@@ -0,0 +1,7 @@
+let userName = "webBrain  webBrain  webBrain  webBrain webBrain webBrain webBrain webBrain webBrain webBrain";
+console.log(userName.replaceAll('web', 'IT'));
+
+let userIsm = prompt('Ism')
+let username = userIsm[userIsm.length - 1]
+let Nurik = username.toLocaleLowerCase() === 'k'
+console.log(Nurik)
